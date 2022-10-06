e9b1fdb0bcdc5f58272a1bb319d1b2aae647e24f
yjyj1023 <lyj19961023@gmail.com>
2022-10-06 20:53:57 +0900

    Set예제(1)

---
+++ Set/src/SetExercise.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExercise {
+    public static void main(String[] args) {
+        Set<String> set1 = new HashSet<>();
+        set1.add("이연재");
+        set1.add("이연재");
+        set1.add("이연재2");
+
+        Set<Integer> setInteger = new HashSet<>();
+        setInteger.add(1);
+        setInteger.add(1);
+        setInteger.add(2);
+        setInteger.add(3);
+
+        System.out.println(setInteger);
+
+    }
+}
